d8e91bdafbe1f8082d9ae925eca1b04b9c81398a
wbxuanyan <469173414@qq.com>
2016-06-05 12:43:48 +0800

    midify

--- guess.py
+++ guess.py
@@ -1,9 +1,10 @@
 from random import randint
 a = randint(1, 10)
 b = input("请输入一个1到10的数字：")
-for i in range(10):
+for i in range(5):
     if b == a:
         print "真聪明，猜对了"
         break
     else:
         b = input("再猜一次：")
+print"游戏结束"
